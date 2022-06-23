7b9e0ff56ce32f46fb73863a4c95b6edc3106c5c
Geromme Beligon <gerommebeligon@Gerommes-MacBook-Air.local>
2022-06-23 15:25:40 -0700

    commit

--- database/database.go
+++ database/database.go
@@ -13,7 +13,7 @@ var err error
 
 func Migration() {
 
-	DSN := ("host=localhost user=postgres password=sparkman13 dbname=banking_system port=5432 sslmode=disable")
+	DSN := ("host=ec2-34-200-35-222.compute-1.amazonaws.com	user=gyiwbhsoxedqid password=8c2e343479959e49e63e6ee94040102f87fae90af23534f2377f87974b0865ed dbname=df6d0e1h8vfbpl port=5432")
 	DB, err = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 	if err != nil {
 		panic(err)
